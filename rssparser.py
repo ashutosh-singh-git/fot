76b3b23121bb20a1952c3675c75de520f200daf7
ashutosh-wynk <ashutosh.singh5335@gmail.com>
2022-10-29 20:19:31 +0530

    added rss parser

--- rssparser.py
+++ rssparser.py
@@ -3,6 +3,8 @@ from pprint import pprint
 import feedparser
 import firebase_admin
 from firebase_admin import db
+from datetime import datetime
+from dateutil import parser
 
 cred_obj = firebase_admin.credentials.Certificate('spotfin-app-firebase-adminsdk-y3hu0-94a797fc1d.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
@@ -26,8 +28,12 @@ post_list = []
 for post in posts:
     temp = dict()
 
+    latest_date = parser.parse(post.published) if latest_date != null
     # if any post doesn't have information then throw error.
     try:
+        #Sat, 29 Oct 2022 18:45:21 +0530
+        # date_time_obj = datetime. strptime(post.published, '%d/%m/%y %H:%M:%S')
+        latest_date = parser.parse(post.published)
         temp["title"] = post.title
         temp["link"] = post.link
         temp["description"] = post.description
@@ -42,6 +48,6 @@ for post in posts:
 
 # storing lists of posts in the dictionary
 posts_details["posts"] = post_list
-ref = db.reference("/")
-ref.set(posts_details)
-pprint(posts_details)
+# ref = db.reference("/")
+# ref.set(posts_details)
+# pprint(posts_details)
